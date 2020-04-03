28c35e13dda5a27fa45c62fb653e230c58ad39d3
Martin Rogge <martin.rogge@sap.com>
2020-04-03 02:01:09 +0200

    new game mechanics

--- android/app/src/main/java/com/mr/four/GameActivity.java
+++ android/app/src/main/java/com/mr/four/GameActivity.java
@@ -7,6 +7,7 @@ import android.animation.*;
 import android.annotation.*;
 import android.content.*;
 import android.graphics.drawable.*;
+import android.media.*;
 import android.os.*;
 import android.preference.*;
 import android.util.*;
@@ -14,18 +15,24 @@ import android.view.*;
 import android.view.animation.*;
 import android.widget.*;
 
-public class GameActivity extends AppCompatActivity implements Runnable {
+import java.util.*;
+
+public class GameActivity extends AppCompatActivity implements Runnable, Animation.AnimationListener {
 
 	private final String PREF_LEVEL = "PrefLevel";
+	private byte maxLevel;
+	private byte currentLevel;
 
 	private final Game game = new Game();
 
 	private byte gameState;
 	private byte color;
+	private byte computerPlayer;
 
-	private TextView level;
-	private TextView message;
-	private View busy;
+	private Spinner levelSpinner;
+	private ArrayAdapter<Integer> levelAdapter;
+	private TextView messageView;
+	private View busyIndicator;
 	private final ImageView[][] images = new ImageView[7][7];
 
 	private final Drawable[] drawables = new Drawable[3];
@@ -35,15 +42,19 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 
 	private final DisplayMetrics displayMetrics = new DisplayMetrics();
 
+	private MediaPlayer mpWon;
+	private MediaPlayer mpLost;
+	private MediaPlayer mpLevelUp0;
+	private MediaPlayer mpLevelUp1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getMetrics(displayMetrics);
-		game.maxLevel = (byte) PreferenceManager.getDefaultSharedPreferences(this).getInt(PREF_LEVEL, 1);
 		setContentView(R.layout.game);
-		level = findViewById(R.id.level);
-		message = findViewById(R.id.message);
-		busy = findViewById(R.id.busy);
+		levelSpinner = findViewById(R.id.level);
+		messageView = findViewById(R.id.message);
+		busyIndicator = findViewById(R.id.busy);
 		ViewGroup box = findViewById(R.id.box);
 		for (int row = 0; row < 7; row++) {
 			ViewGroup rowView = (ViewGroup) box.getChildAt(6 - row);
@@ -58,10 +69,48 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 		winAnimation.setDuration(500);
 		winAnimation.setRepeatMode(ValueAnimator.REVERSE);
 		winAnimation.setRepeatCount(ValueAnimator.INFINITE);
+		mpWon = MediaPlayer.create(this, R.raw.won);
+		mpLost = MediaPlayer.create(this, R.raw.lost);
+		mpLevelUp0 = MediaPlayer.create(this, R.raw.level_up_0);
+		mpLevelUp1 = MediaPlayer.create(this, R.raw.level_up_1);
+		loadLevel();
+		game.maxLevel = currentLevel;
+		//levelAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item);
+		levelAdapter = new ArrayAdapter<Integer>(this, R.layout.spinner_item, R.id.spinnerItem);
+		for (int i = 1 ; i <= maxLevel; i++)
+			levelAdapter.add(i);
+		levelSpinner.setAdapter(levelAdapter);
+		levelSpinner.setSelection(currentLevel - 1);
+		levelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				game.maxLevel = currentLevel = (byte) (position + 1);
+				saveLevel();
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) { }
+		});
 		//
 		gameState = 4;
 		color = Game.WHITE;
+		computerPlayer = Game.BLACK;
 		setMessage();
+	}
+
+	private void loadLevel() {
+		int packedLevel = PreferenceManager.getDefaultSharedPreferences(this).getInt(PREF_LEVEL, 0);
+		maxLevel = (byte) (packedLevel >> 8);
+		currentLevel = (byte) (packedLevel & 0xff);
+		if (maxLevel < 1)
+			maxLevel = 1;
+		if (currentLevel > maxLevel)
+			currentLevel = maxLevel;
+	}
+
+	private void saveLevel() {
+		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+		editor.putInt(PREF_LEVEL, (maxLevel << 8) | currentLevel);
+		editor.apply();
 	}
 
 	private static final int[] messageText = {
@@ -74,41 +123,45 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 
 	@SuppressLint("SetTextI18n")
 	private void setMessage() {
-		if (gameState == Game.RUNNING && color == Game.BLACK) {
-			message.setVisibility(View.INVISIBLE);
-			busy.setVisibility(View.VISIBLE);
+		if (gameState == Game.RUNNING && color == computerPlayer) {
+			messageView.setVisibility(View.INVISIBLE);
+			busyIndicator.setVisibility(View.VISIBLE);
 		} else {
-			message.setText(messageText[gameState]);
-			message.setVisibility(View.VISIBLE);
-			busy.setVisibility(View.INVISIBLE);
+			messageView.setText(messageText[gameState]);
+			messageView.setVisibility(View.VISIBLE);
+			busyIndicator.setVisibility(View.INVISIBLE);
 		}
-		level.setText(Integer.toString(game.maxLevel));
 	}
 
 	private void nextMove(byte column) {
-		drop(column, color);
+		drop(color, column, game.getTop(column));
 		game.drop(column, color);
 		gameState = winner(column);
 		switch (gameState) {
 			case Game.RUNNING:
 				color = Game.opposite(color);
 				setMessage();
-				if (color == Game.BLACK)
+				if (color == computerPlayer)
 					new SearchTask().execute();
 				break;
 			case Game.WHITE:
-				level.postDelayed(this, 600);
+				messageView.postDelayed(this, 750);
 				setMessage();
-				game.maxLevel++;
-				saveLevel();
+				if (currentLevel >= maxLevel) {
+					(currentLevel % 2 == 0 ? mpLevelUp0 : mpLevelUp1).start();
+					game.maxLevel = currentLevel = ++maxLevel;
+					saveLevel();
+					levelAdapter.add((int) maxLevel);
+					levelAdapter.notifyDataSetChanged();
+					levelSpinner.setSelection(currentLevel - 1);
+					// TODO: Level up animation
+				} else
+					mpWon.start();
 				break;
 			case Game.BLACK:
-				level.postDelayed(this, 600);
+				messageView.postDelayed(this, 750);
 				setMessage();
-				if (game.maxLevel > 0) {
-					game.maxLevel--;
-					saveLevel();
-				}
+				mpLost.start();
 				break;
 			default:
 				setMessage();
@@ -143,18 +196,13 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 		} catch(Exception ignored) { }
 	}
 
-	private void saveLevel() {
-		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-		editor.putInt(PREF_LEVEL, game.maxLevel);
-		editor.apply();
-	}
-
-	private void clearBoard() { // TODO: any effects?
+	private void clearBoard() {
 		for (int row = 6; row >= 0; row--)
 			for (int column = 0; column < 7; column++) {
 				images[row][column].setAlpha(1F);
 				images[row][column].setImageDrawable(null);
 			}
+		// TODO: any effects?
 	}
 
 	private Animation getFallAnimation(int row) {
@@ -171,18 +219,52 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 		return animationSet;
 	}
 
-	private void drop(byte column, byte color) { // TODO: more effects?
-		int row = game.getTop(column);
-		ImageView image = images[row][column];
-		image.setImageDrawable(drawables[color]);
-		image.setColorFilter(colors[color]);
-		image.startAnimation(getFallAnimation(row));
+	private final LinkedList<Integer> drops = new LinkedList<>();
+
+	private boolean canDrop = true;
+
+	@Override
+	public void onAnimationStart(Animation animation) { }
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		synchronized (drops) {
+			canDrop = true;
+			dropNext(drops.poll());
+		}
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) { }
+
+	private void dropNext(Integer stone) {
+		if (stone == null)
+			return;
+		if (canDrop) {
+			canDrop = false;
+			byte color = (byte) (stone >> 16);
+			byte column = (byte) ((stone >> 8) & 0xff);
+			byte row = (byte) (stone & 0xff);
+			ImageView image = images[row][column];
+			image.setImageDrawable(drawables[color]);
+			image.setColorFilter(colors[color]);
+			Animation fallAnimation = getFallAnimation(row);
+			fallAnimation.setAnimationListener(this);
+			image.startAnimation(fallAnimation);
+		} else
+			drops.offer(stone);
+	}
+
+	private void drop(byte color, byte column, byte row) {
+		synchronized (drops) {
+			dropNext((color << 16) | (column << 8) | row);
+		}
 	}
 
 	private void tryColumn(byte column) {
 		if (gameState == 4)
 			startGame();
-		if (gameState == Game.RUNNING && color == Game.WHITE && game.isOption(column))
+		if (gameState == Game.RUNNING && color != computerPlayer && game.isOption(column))
 			nextMove(column);
 	}
 
@@ -218,10 +300,14 @@ public class GameActivity extends AppCompatActivity implements Runnable {
 		winAnimation.removeAllUpdateListeners();
 		if (winAnimation.isRunning())
 			winAnimation.end();
+		drops.clear();
+		canDrop = true;
 		game.init();
 		gameState = Game.RUNNING;
 		color = Game.WHITE;
 		clearBoard();
+		if (color == computerPlayer)
+			new SearchTask().execute();
 	}
 
 	public void click(View v) {
